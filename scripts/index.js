fdd5b0b442c41c1c7a9fdc73b706647525d19897
catarak <ctarakajian@gmail.com>
2016-05-25 17:30:57 -0400

    initial commit

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+var instrumental = new Tone.Player({
+	"url" : "sounds/instrumental.wav"
+}).toMaster();
+
+$(function() {
+	$("#play-button").click(function() {
+		Tone.Transport.start();
+		instrumental.start();
+		$(this).toggleClass("hidden");
+		$("#stop-button").toggleClass("hidden");
+	});
+
+	$("#stop-button").click(function() {
+		instrumental.stop();
+		Tone.Transport.stop();
+		$(this).toggleClass("hidden");
+		$("#play-button").toggleClass("hidden");
+	});
+});
